4fc7888ee9af084fe6bce3f38d273ca155d5e12c
MongusaEye <186921147+MongusaEye@users.noreply.github.com>
2025-12-30 01:36:55 -0800

    removed the magic numbers from emotesounds

--- Common/EmoteSounds.cs
+++ Common/EmoteSounds.cs
@@ -18,72 +18,48 @@ public class EmoteSounds : GlobalEmoteBubble
             return;
         // add modded emote sounds here
         // Options: "Neuro", "Evil", "Twins", "TwinsAndPlayer"
-        PlayModEmoteSound<NeweroEmote>(bubble, "fx_groantube", "Twins");
-        PlayModEmoteSound<NewlivEmote>(bubble, "fx_metalpipe", "Evil"); //temporary until pipes emote is drawn
+        PlayEmoteSound(ModContent.EmoteBubbleType<NeweroEmote>(), bubble, "fx_groantube", "Twins");
+        PlayEmoteSound(ModContent.EmoteBubbleType<NewlivEmote>(), bubble, "fx_metalpipe", "Evil"); //temporary until pipes emote is drawn
+        PlayEmoteSound(EmoteID.EmoteSadness, bubble, "fx_wompwompwomp", "Twins");
+        PlayEmoteSound(EmoteID.EmoteSilly, bubble, "fx_clownhorn", "Twins");
+        PlayEmoteSound(EmoteID.DebuffCurse, bubble, "fx_incorrectbuzzer", "Twins");
+        PlayEmoteSound(EmoteID.EmoteNote, bubble, "fx_correctbuzzer", "Twins");
+        PlayEmoteSound(EmoteID.ItemDynamite, bubble, "fx_vineboom", "Twins");
+        PlayEmoteSound(EmoteID.DebuffSilence, bubble, "fx_awkwardcrickets", "Twins"); //silent (the emote is called silent but the sound is not)
+        PlayEmoteSound(EmoteID.EmotionLove, bubble, "fx_rizz", "Twins");
+        PlayEmoteSound(EmoteID.EmoteScowl, bubble, "fx_scaryviolin", "Twins");
+        PlayEmoteSound(EmoteID.ItemSword, bubble, "fx_poke", "Twins");
+        PlayEmoteSound(EmoteID.CritterSlime, bubble, "fx_sus", "Twins"); //(it looks like the visor of the crewmate trust)
+        PlayEmoteSound(EmoteID.ItemTombstone, bubble, "fx_deathbell", "Twins");
+        PlayEmoteSound(EmoteID.EmoteLaugh, bubble, "fx_badumtss", "Twins");
             // voicelines
-        PlayModEmoteSound<ErmEmote>(bubble, "evil_erm", "Evil");
-        PlayModEmoteSound<ErmEmote>(bubble, "neuro_erm", "Neuro");
-        
-        PlayModEmoteSound<HeartEmote>(bubble, "evil_heart", "Evil");
-        PlayModEmoteSound<HeartEmote>(bubble, "neuro_heart!!", "Neuro");
+        PlayEmoteSound(ModContent.EmoteBubbleType<ErmEmote>(), bubble, "evil_erm", "Evil");
+        PlayEmoteSound(ModContent.EmoteBubbleType<ErmEmote>(), bubble, "neuro_erm", "Neuro");
 
-        PlayModEmoteSound<FrickEmote>(bubble, "evil_frick", "Evil");
-        PlayModEmoteSound<FrickEmote>(bubble, "neuro_frick", "Neuro");
+        PlayEmoteSound(ModContent.EmoteBubbleType<HeartEmote>(), bubble, "evil_heart", "Evil");
+        PlayEmoteSound(ModContent.EmoteBubbleType<HeartEmote>(), bubble, "neuro_heart!!", "Neuro");
 
-        PlayModEmoteSound<SmileEmote>(bubble, "evil_smile", "Evil");
-        PlayModEmoteSound<SmileEmote>(bubble, "neuro_smile", "Neuro");
+        PlayEmoteSound(ModContent.EmoteBubbleType<FrickEmote>(), bubble, "evil_frick", "Evil");
+        PlayEmoteSound(ModContent.EmoteBubbleType<FrickEmote>(), bubble, "neuro_frick", "Neuro");
 
-        PlayModEmoteSound<FocusEmote>(bubble, "evil_focus!!", "Evil");
-        PlayModEmoteSound<FocusEmote>(bubble, "neuro_hmm!", "Neuro");
+        PlayEmoteSound(ModContent.EmoteBubbleType<SmileEmote>(), bubble, "evil_smile", "Evil");
+        PlayEmoteSound(ModContent.EmoteBubbleType<SmileEmote>(), bubble, "neuro_smile", "Neuro");
 
-        // add vanilla emote sounds here
-        PlayVanillaEmoteSound(134, bubble, "fx_wompwompwomp", "Twins"); //sad
-        PlayVanillaEmoteSound(139, bubble, "fx_clownhorn", "Twins"); //silly
-        PlayVanillaEmoteSound(11, bubble, "fx_incorrectbuzzer", "Twins"); //curse 
-        PlayVanillaEmoteSound(17, bubble, "fx_correctbuzzer", "Twins"); //music
-        PlayVanillaEmoteSound(81, bubble, "fx_vineboom", "Twins"); //dynamite
-        PlayVanillaEmoteSound(10, bubble, "fx_awkwardcrickets", "Twins"); //silent (the emote is called silent but the sound is not)
-        PlayVanillaEmoteSound(0, bubble, "fx_rizz", "Twins"); //heart (red)
-        PlayVanillaEmoteSound(138, bubble, "fx_scaryviolin", "Twins"); //scowl
-        PlayVanillaEmoteSound(78, bubble, "fx_poke", "Twins"); //sword
-        PlayVanillaEmoteSound(13, bubble, "fx_sus", "Twins"); //slime (it looks like the visor of the crewmate trust)
-        PlayVanillaEmoteSound(84, bubble, "fx_deathbell", "Twins"); //tombstone
-        PlayVanillaEmoteSound(15, bubble, "fx_badumtss", "Twins"); //laugh
+        PlayEmoteSound(ModContent.EmoteBubbleType<FocusEmote>(), bubble, "evil_focus!!", "Evil");
+        PlayEmoteSound(ModContent.EmoteBubbleType<FocusEmote>(), bubble, "neuro_hmm!", "Neuro");
+
     }
 
-    private void PlayModEmoteSound<T>(
-        EmoteBubble bubble,
-        string soundName,
-        string speaker
-    ) where T : ModEmoteBubble
-    {
-        int emoteType = ModContent.EmoteBubbleType<T>();
-
-        if (bubble.emote != emoteType)
-            return;
-
-        PlayEmoteSound(bubble, soundName, speaker);
-    }
-
-     private void PlayVanillaEmoteSound(
+    private void PlayEmoteSound(
         int emoteID,
         EmoteBubble bubble,
         string soundName,
         string speaker
     )
     {
+
         if (bubble.emote != emoteID)
             return;
-
-        PlayEmoteSound(bubble, soundName, speaker);
-    }
-
-    private void PlayEmoteSound(
-        EmoteBubble bubble,
-        string soundName,
-        string speaker
-    )
-    {
 
         if (bubble.anchor.entity is not Player && bubble.anchor.entity is not NPC)
             return;
