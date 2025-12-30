02751f33551957c2aad1981b316a5fd74381e1a3
Konomi <konomik14@gmail.com>
2025-12-31 04:12:45 +1100

    convert 2 volume sliders into 1 and a toggle for each (+ slight refactor)

--- Common/EmoteSounds.cs
+++ Common/EmoteSounds.cs
@@ -25,42 +25,48 @@ public class EmoteSounds : GlobalEmoteBubble
         if (Main.netMode == NetmodeID.Server)
             return;
 
-        if (ModContent.GetInstance<NeurosamaConfig>().EmoteSoundsToggle == false)
+        if (ModContent.GetInstance<NeurosamaConfig>().EmoteSoundsVolume == 0f)
+            // Emote sound volume is 0, skip emote sound playback
             return;
 
-        PlayEmoteSound(ModContent.EmoteBubbleType<NeweroEmote>(), bubble, "SFX/groantube", EmoteOrigin.Twins);
-        PlayEmoteSound(ModContent.EmoteBubbleType<NewlivEmote>(), bubble, "SFX/metalpipe", EmoteOrigin.Evil); //temporary until pipes emote is drawn
-        PlayEmoteSound(EmoteID.EmoteSadness, bubble, "SFX/wompwompwomp", EmoteOrigin.Twins);
-        PlayEmoteSound(EmoteID.EmoteSilly, bubble, "SFX/clownhorn", EmoteOrigin.Twins);
-        PlayEmoteSound(EmoteID.DebuffCurse, bubble, "SFX/incorrectbuzzer", EmoteOrigin.Twins);
-        PlayEmoteSound(EmoteID.EmoteNote, bubble, "SFX/correctbuzzer", EmoteOrigin.Twins);
-        PlayEmoteSound(EmoteID.ItemDynamite, bubble, "SFX/vineboom", EmoteOrigin.Twins);
-        PlayEmoteSound(EmoteID.DebuffSilence, bubble, "SFX/awkwardcrickets", EmoteOrigin.Twins);
-        PlayEmoteSound(EmoteID.EmoteKiss, bubble, "SFX/rizz", EmoteOrigin.Twins); // temporary until rizz emote is drawn (will not be used currently because not in default emote pool)
-        PlayEmoteSound(EmoteID.EmoteFear, bubble, "SFX/scaryviolin", EmoteOrigin.Twins);
-        //PlayEmoteSound(EmoteID.ItemSword, bubble, "SFX/poke", EmoteOrigin.Twins); 
-        PlayEmoteSound(EmoteID.CritterSlime, bubble, "SFX/sus", EmoteOrigin.Twins); //(it looks like the visor of the crewmate trust)
-        PlayEmoteSound(EmoteID.ItemTombstone, bubble, "SFX/deathbell", EmoteOrigin.Twins); // extremely rare, only used by default when like 5% health which they will probably die or heal before using)
-        PlayEmoteSound(EmoteID.EmoteLaugh, bubble, "SFX/badumtss", EmoteOrigin.Twins);
+        if (ModContent.GetInstance<NeurosamaConfig>().EmoteSFXToggle) 
+        {
+            PlayEmoteSound(ModContent.EmoteBubbleType<NeweroEmote>(), bubble, "SFX/groantube", EmoteOrigin.Twins);
+            PlayEmoteSound(ModContent.EmoteBubbleType<NewlivEmote>(), bubble, "SFX/metalpipe", EmoteOrigin.Evil); //temporary until pipes emote is drawn
+            PlayEmoteSound(EmoteID.EmoteSadness, bubble, "SFX/wompwompwomp", EmoteOrigin.Twins);
+            PlayEmoteSound(EmoteID.EmoteSilly, bubble, "SFX/clownhorn", EmoteOrigin.Twins);
+            PlayEmoteSound(EmoteID.DebuffCurse, bubble, "SFX/incorrectbuzzer", EmoteOrigin.Twins);
+            PlayEmoteSound(EmoteID.EmoteNote, bubble, "SFX/correctbuzzer", EmoteOrigin.Twins);
+            PlayEmoteSound(EmoteID.ItemDynamite, bubble, "SFX/vineboom", EmoteOrigin.Twins);
+            PlayEmoteSound(EmoteID.DebuffSilence, bubble, "SFX/awkwardcrickets", EmoteOrigin.Twins);
+            PlayEmoteSound(EmoteID.EmoteKiss, bubble, "SFX/rizz", EmoteOrigin.Twins); // temporary until rizz emote is drawn (will not be used currently because not in default emote pool)
+            PlayEmoteSound(EmoteID.EmoteFear, bubble, "SFX/scaryviolin", EmoteOrigin.Twins);
+            //PlayEmoteSound(EmoteID.ItemSword, bubble, "SFX/poke", EmoteOrigin.Twins); 
+            PlayEmoteSound(EmoteID.CritterSlime, bubble, "SFX/sus", EmoteOrigin.Twins); //(it looks like the visor of the crewmate trust)
+            PlayEmoteSound(EmoteID.ItemTombstone, bubble, "SFX/deathbell", EmoteOrigin.Twins); // extremely rare, only used by default when like 5% health which they will probably die or heal before using)
+            PlayEmoteSound(EmoteID.EmoteLaugh, bubble, "SFX/badumtss", EmoteOrigin.Twins);
+        }
 
-        // Voicelines
-        PlayEmoteSound(ModContent.EmoteBubbleType<ErmEmote>(), bubble, "evil_erm", EmoteOrigin.Evil);
-        PlayEmoteSound(ModContent.EmoteBubbleType<ErmEmote>(), bubble, "neuro_erm", EmoteOrigin.Neuro);
+        if (ModContent.GetInstance<NeurosamaConfig>().EmoteVoiceToggle)
+        {
+            PlayEmoteSound(ModContent.EmoteBubbleType<ErmEmote>(), bubble, "evil_erm", EmoteOrigin.Evil);
+            PlayEmoteSound(ModContent.EmoteBubbleType<ErmEmote>(), bubble, "neuro_erm", EmoteOrigin.Neuro);
 
-        PlayEmoteSound(ModContent.EmoteBubbleType<HeartEmote>(), bubble, "evil_heart", EmoteOrigin.Evil);
-        PlayEmoteSound(ModContent.EmoteBubbleType<HeartEmote>(), bubble, "neuro_heart!!", EmoteOrigin.Neuro);
-        PlayEmoteSound(ModContent.EmoteBubbleType<FrickEmote>(), bubble, "evil_frick", EmoteOrigin.Evil);
-        PlayEmoteSound(ModContent.EmoteBubbleType<FrickEmote>(), bubble, "neuro_frick", EmoteOrigin.Neuro);
+            PlayEmoteSound(ModContent.EmoteBubbleType<HeartEmote>(), bubble, "evil_heart", EmoteOrigin.Evil);
+            PlayEmoteSound(ModContent.EmoteBubbleType<HeartEmote>(), bubble, "neuro_heart!!", EmoteOrigin.Neuro);
+            PlayEmoteSound(ModContent.EmoteBubbleType<FrickEmote>(), bubble, "evil_frick", EmoteOrigin.Evil);
+            PlayEmoteSound(ModContent.EmoteBubbleType<FrickEmote>(), bubble, "neuro_frick", EmoteOrigin.Neuro);
 
-        PlayEmoteSound(ModContent.EmoteBubbleType<SmileEmote>(), bubble, "evil_smile", EmoteOrigin.Evil);
-        PlayEmoteSound(ModContent.EmoteBubbleType<SmileEmote>(), bubble, "neuro_smile", EmoteOrigin.Neuro);
+            PlayEmoteSound(ModContent.EmoteBubbleType<SmileEmote>(), bubble, "evil_smile", EmoteOrigin.Evil);
+            PlayEmoteSound(ModContent.EmoteBubbleType<SmileEmote>(), bubble, "neuro_smile", EmoteOrigin.Neuro);
 
-        PlayEmoteSound(ModContent.EmoteBubbleType<FocusEmote>(), bubble, "evil_focus!!", EmoteOrigin.Evil);
-        PlayEmoteSound(ModContent.EmoteBubbleType<FocusEmote>(), bubble, "neuro_hmm!", EmoteOrigin.Neuro);
+            PlayEmoteSound(ModContent.EmoteBubbleType<FocusEmote>(), bubble, "evil_focus!!", EmoteOrigin.Evil);
+            PlayEmoteSound(ModContent.EmoteBubbleType<FocusEmote>(), bubble, "neuro_hmm!", EmoteOrigin.Neuro);
+        }
 
     }
 
-    private void PlayEmoteSound(
+    public static void PlayEmoteSound(
         int emoteID,
         EmoteBubble bubble,
         string soundName,
@@ -91,14 +97,7 @@ public class EmoteSounds : GlobalEmoteBubble
                 return;
         }
 
-        float volume = 0.5f;
-        if (soundName.StartsWith("SFX"))
-        {
-            volume = ModContent.GetInstance<NeurosamaConfig>().EmoteSoundsVolumeFX;
-        } else
-        {
-            volume = ModContent.GetInstance<NeurosamaConfig>().EmoteSoundsVolumeVoice;
-        }
+        float volume = ModContent.GetInstance<NeurosamaConfig>().EmoteSoundsVolume;
         SoundEngine.PlaySound(
             new SoundStyle($"{nameof(Neurosama)}/Assets/Sounds/{soundName}") {Volume = volume},
             bubble.anchor.entity.Center
