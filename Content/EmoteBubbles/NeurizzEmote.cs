71a59ba0c91d372c20187b6c456d4a7f43adee29
MongusaEye <186921147+MongusaEye@users.noreply.github.com>
2025-12-30 17:30:54 -0800

    add neurizz emote

---
+++ Content/EmoteBubbles/NeurizzEmote.cs
@@ -0,0 +1,13 @@
+﻿using Terraria.GameContent.UI;
+using Terraria.ModLoader;
+
+namespace Neurosama.Content.EmoteBubbles
+{
+    public class NeurizzEmote : ModEmoteBubble
+    {
+        public override void SetStaticDefaults()
+        {
+            AddToCategory(EmoteID.Category.General);
+        }
+    }
+}
